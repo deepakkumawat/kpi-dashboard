1cc27da0ccff6dcc4029d1071cc1f0a0c9bdfb97
DeepakK <deepaksinghkumawat@gmail.com>
2021-10-03 14:21:51 +0530

    Layout changes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kpi-dashboard
 KPI Dashboard for visualising the marketing data
+
+## Technologies used
+ - Java 8
+ - Spring boot
+ - Spring batch
+ - H2 database
+ - AngularJS
+ - Angular material
+ - Google chart
+ - aws ec2
+
--- src/main/resources/static/js/kpiApp.js
+++ src/main/resources/static/js/kpiApp.js
@@ -1,74 +1,96 @@
 angular.module('kpiApp', ['ngMaterial', 'ngMessages'])
-  .controller('KPIDashboardController', function($scope, $http, $filter) {
-    // Google chart load
-    google.charts.load('current', {'packages':['corechart', 'bar']});
-    google.charts.setOnLoadCallback(setupInitialDashboard);
+    .controller('KPIDashboardController', function ($scope, $http, $filter) {
+        // Google chart load
+        google.charts.load('current', { 'packages': ['corechart', 'bar'] });
+        google.charts.setOnLoadCallback(setupInitialDashboard);
 
-    var kpiDashboard = this;
-    var defaultBrand = "ABC";
-    var barChartHeaders = ['Date', 'Orders', 'Gross revenue', 'Gross margin', 'Add to carts', 'Customer acquired', 'Clicks', 'Marketing cost(USD)'];
-    var aovCvrChartHeaders = ['Date', 'AOV', 'CVR'];
-    var barChartOptions = {
-      chart: {
-        title: 'Brand Performance',
-        subtitle: 'KPI Dashboard',
-      }
-    };
+        var kpiDashboard = this;
+        var defaultBrand = "ABC";
+        var barChart, aovCvrChart, carCacCart, apmRpcChart;
+        var barChartHeaders = ['Date', 'Orders', 'Gross revenue', 'Gross margin', 'Add to carts', 'Customer acquired', 'Clicks', 'Marketing cost(USD)'];
+        var aovCvrChartHeaders = ['Date', 'AOV', 'CVR'];
+        var carCacChartHeaders = ['Date', 'CAC', 'CAR'];
+        var apmRpcChartHeaders = ['Date', 'APM', 'RPC'];
+        var barChartOptions = {
+            chart: {
+                title: 'Brand Performance',
+                subtitle: 'KPI Dashboard',
+            }
+        };
+        var aovCvrChartOptions = {
+            title: 'AOV CVR Timeline',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+        var carCacCartOptions = {
+            title: 'CAR CAC Timeline',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+        var apmRpcChartOptions = {
+            title: 'APM RPC Timeline',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+        kpiDashboard.brands = [{ val: defaultBrand }];
 
-    var aovCvrChartOptions = {
-      title: 'AOV Timeline',
-      curveType: 'function',
-      legend: { position: 'bottom' }
-    };
-    var barChart, aovCvrChart;
-    kpiDashboard.brands = [{val: defaultBrand}];
+        function setupInitialDashboard() {
+            console.log("Google charts loaded, making initial data load");
 
-    function setupInitialDashboard() {
-      console.log("Google charts loaded, making initial data load");
+            // Initial chart setup
+            barChart = new google.charts.Bar(document.getElementById('wholeDataChart'));
+            barChart.draw(google.visualization.arrayToDataTable([barChartHeaders].concat([[0, 0, 0, 0, 0, 0, 0, 0]])), google.charts.Bar.convertOptions(barChartOptions));
 
-      // Initial chart setup
-      barChart = new google.charts.Bar(document.getElementById('wholeDataChart'));
-      barChart.draw(google.visualization.arrayToDataTable([barChartHeaders]), google.charts.Bar.convertOptions(barChartOptions));
-      aovCvrChart = new google.visualization.LineChart(document.getElementById('aov_cvr_DataCart'));
-      aovCvrChart.draw(google.visualization.arrayToDataTable([aovCvrChartHeaders]), aovCvrChartOptions);
+            aovCvrChart = new google.visualization.LineChart(document.getElementById('aov_cvr_DataCart'));
+            aovCvrChart.draw(google.visualization.arrayToDataTable([aovCvrChartHeaders].concat([[0, 0, 0]])), aovCvrChartOptions);
 
-      //Loading data for last 10 days
-      var _toDate = new Date();
-      var _fromDate = new Date();
-      _fromDate.setDate(_fromDate.getDate()-10);
-      kpiDashboard.fromDate = _fromDate;
-      kpiDashboard.toDate = _toDate;
-      kpiDashboard.brand = defaultBrand;
-      var fromDate = _fromDate.toISOString().substring(0,10);
-      var toDate = _toDate.toISOString().substring(0,10);
-      kpiDashboard.loadDate(defaultBrand, fromDate, toDate);
-    }
+            carCacCart = new google.visualization.LineChart(document.getElementById('car_cac_DataCart'));
+            carCacCart.draw(google.visualization.arrayToDataTable([carCacChartHeaders].concat([[0, 0, 0]])), carCacCartOptions);
 
-    kpiDashboard.loadDate = function(brand, fromDate, toDate) {
-      console.log("Going to load the data from rest for brand: " + brand + ", fromDate: " + fromDate + ", toDate: " + toDate);
+            apmRpcChart = new google.visualization.LineChart(document.getElementById('apm_rpc_DataCart'));
+            apmRpcChart.draw(google.visualization.arrayToDataTable([apmRpcChartHeaders].concat([[0, 0, 0]])), apmRpcChartOptions);
 
-      $http.post('/kpi/data/' + brand, {"fromDate": fromDate, "toDate": toDate}).
-        then(function(_data) {
-          var chart1Data = _data.data.map(rec => {
-            return [rec.date, rec.orders, rec.grossRevenue, rec.grossMargin, rec.addToCarts, rec.customersAcquired, rec.clicks, rec.marketingCost];
-          });
+            //Loading data for last 10 days
+            var _toDate = new Date();
+            var _fromDate = new Date();
+            _fromDate.setDate(_fromDate.getDate() - 10);
+            kpiDashboard.fromDate = _fromDate;
+            kpiDashboard.toDate = _toDate;
+            kpiDashboard.brand = defaultBrand;
+            var fromDate = _fromDate.toISOString().substring(0, 10);
+            var toDate = _toDate.toISOString().substring(0, 10);
+            kpiDashboard.loadDate(defaultBrand, fromDate, toDate);
+        }
 
-          var aovCvrChartData = _data.data.map(rec => {
-            return [rec.date, rec.aov, rec.cvr];
-          });
+        kpiDashboard.loadDate = function (brand, fromDate, toDate) {
+            console.log("Going to load the data from rest for brand: " + brand + ", fromDate: " + fromDate + ", toDate: " + toDate);
 
-          barChart.draw(google.visualization.arrayToDataTable([barChartHeaders].concat(chart1Data)), barChartOptions);
-          aovCvrChart.draw(google.visualization.arrayToDataTable([aovCvrChartHeaders].concat(aovCvrChartData)), aovCvrChartOptions);
-      });
-    }
+            $http.post('/kpi/data/' + brand, { "fromDate": fromDate, "toDate": toDate }).
+                then(function (_data) {
+                    var barCharData = [];
+                    var aovCvrChartData = [];
+                    var carCacCartData = [];
+                    var apmRpcChartData = [];
+                    _data.data.forEach(rec => {
+                        barCharData.push([rec.date, rec.orders, rec.grossRevenue, rec.grossMargin, rec.addToCarts, rec.customersAcquired, rec.clicks, rec.marketingCost]);
+                        aovCvrChartData.push([rec.date, rec.aov, rec.cvr]);
+                        carCacCartData.push([rec.date, rec.car, rec.cac]);
+                        apmRpcChartData.push([rec.date, rec.apm, rec.rpc]);
+                    });
+                    barChart.draw(google.visualization.arrayToDataTable([barChartHeaders].concat(barCharData)), barChartOptions);
+                    aovCvrChart.draw(google.visualization.arrayToDataTable([aovCvrChartHeaders].concat(aovCvrChartData)), aovCvrChartOptions);
+                    carCacCart.draw(google.visualization.arrayToDataTable([carCacChartHeaders].concat(carCacCartData)), carCacCartOptions);
+                    apmRpcChart.draw(google.visualization.arrayToDataTable([apmRpcChartHeaders].concat(carCacCartData)), carCacCartOptions);
+                });
+        }
 
-    kpiDashboard.fetchData = function() {
-        var fromDate = kpiDashboard.fromDate.toISOString().substring(0, 10);
-        var toDate = kpiDashboard.toDate.toISOString().substring(0, 10);
-        console.log("Brand: " + kpiDashboard.brand);
-        console.log("fromDate: " + fromDate);
-        console.log("toDate: " + toDate);
-      kpiDashboard.dataFilters = "Brand: " + kpiDashboard.brand + ", From Date: " + fromDate + ", To Date: " + toDate;
-      kpiDashboard.loadDate(defaultBrand, fromDate, toDate);
-    };
-  });
+        kpiDashboard.fetchData = function () {
+            var fromDate = kpiDashboard.fromDate.toISOString().substring(0, 10);
+            var toDate = kpiDashboard.toDate.toISOString().substring(0, 10);
+            console.log("Brand: " + kpiDashboard.brand);
+            console.log("fromDate: " + fromDate);
+            console.log("toDate: " + toDate);
+            kpiDashboard.dataFilters = "Brand: " + kpiDashboard.brand + ", From Date: " + fromDate + ", To Date: " + toDate;
+            kpiDashboard.loadDate(defaultBrand, fromDate, toDate);
+        };
+    });
